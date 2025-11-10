40d6ccc60d410559ca63ea6c4fdb57ffae1a263f
Gabriel Massola <154900620+MassolaGabriel@users.noreply.github.com>
2025-11-10 14:50:07 -0300

    Add files via upload

---
+++ aula 18 - spring security/swagger-senai/src/main/java/swagger/SwaggerSenaiApplication.java
@@ -0,0 +1,13 @@
+package swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerSenaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerSenaiApplication.class, args);
+	}
+
+}
