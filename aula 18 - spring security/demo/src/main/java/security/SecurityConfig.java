2dbf9b1877049c34d5d0458ce0a37baaf6c2fcc6
Gabriel Massola <154900620+MassolaGabriel@users.noreply.github.com>
2025-11-10 14:18:54 -0300

    Add files via upload

---
+++ aula 18 - spring security/demo/src/main/java/security/SecurityConfig.java
@@ -0,0 +1,56 @@
+package security;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+	
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+	    http
+	        .httpBasic(withDefaults())
+	        .authorizeHttpRequests(authz -> authz
+	        	.requestMatchers("/api/publico").permitAll()
+	            .requestMatchers("/api/usuarios/**").hasRole("USER")  
+	            .anyRequest().hasAnyRole("ADMIN", "USER") 
+	        )
+	        .csrf(csrf -> csrf.disable());
+	    return http.build();
+	}
+	 
+	 @Bean
+	    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+	        UserDetails user1 = User.withUsername("user1")
+	            .password(passwordEncoder.encode("senha1"))
+	            .roles("USER")
+	            .build();
+
+	        UserDetails admin = User.withUsername("admin")
+	            .password(passwordEncoder.encode("senha2"))
+	            .roles("ADMIN")
+	            .build();
+
+	        return new InMemoryUserDetailsManager(user1, admin);
+	    }
+
+}
--- aula 18 - spring security/demo/src/main/java/security/StartController.java
+++ aula 18 - spring security/demo/src/main/java/security/StartController.java
@@ -1,5 +1,6 @@
 package security;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +9,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class StartController {
 
-	@GetMapping
+	@GetMapping("/publico")
 	public String publico() {
 		return "rota publica";
 	}
+	
+	@GetMapping("/privada")
+	public String privada() {
+		return "rota privada, necessario acesso";
+	}
+	
+	@GetMapping("/admin")
+	@PreAuthorize("hasRole('ADMIN')")
+	public String administrador() {
+		return "Rota privada, necessario acesso admin";
+	}
+	
 }
